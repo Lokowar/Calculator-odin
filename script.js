f4e28a0c708022ad30c810bed4436054824da55c
Lokowar <lokwoar1@gmail.com>
2022-07-07 17:46:20 -0300

    0.3 - Adicionei capacidade de operações basicas

--- script.js
+++ script.js
@@ -1,31 +1,77 @@
 $(document).ready(function() {
     let x = "";
-    let screenOp = document.getElementById('screen1');
-    let screenRes = document.getElementById('screen2');
-    let msc = document.getElementsByClassName("msc-btn");
+    let y;
+    let resolution;
+
+    let nmb1 = document.getElementById('screen1');
+    let op = document.getElementById('screen2');
+    let nmb2 = document.getElementById('screen3');
+    let res = document.getElementById('screen4');
+
+    function soma(x, y){
+        return x + y;
+    }
+    function subtração(x, y){
+        return x - y;
+    }
+    function multiplicação(x, y){
+        return x * y;
+    }
+    function divisão(x, y){
+        return x / y;
+    }    
 
     $(".btn").click(function(){
-        if(typeof this.textContent === "string"){
+        if(this.textContent === "+" || this.textContent === "-" || this.textContent === "*" || this.textContent === "/"){
+            y = x;
+            x = "";
+            nmb1.innerHTML = x;
+            nmb2.innerHTML = y;
+            op.innerHTML = this.textContent;
+            return;
+        }
+        if(this.textContent === "="){
+            if(op.innerHTML === "+"){
+                resolution = soma(x, y);
+                res.innerHTML += resolution;
+                return;
+            }
+            if(op.innerHTML === "-"){
+                resolution = subtração(x, y);
+                res.innerHTML += resolution;
+                return;
+            }
+            if(op.innerHTML === "*"){
+                resolution = multiplicação(x, y);
+                res.innerHTML += resolution;
+                return;
+            }
+            if(op.innerHTML === "/"){
+                resolution = divisão(x, y);
+                res.innerHTML += resolution;
+                return;
+            }
+        }else{
             x += this.textContent;
             x = parseInt(x);
-            screenOp.innerHTML = x;
-        }
-        if(this.textContent === "+"){
-            x += this.textContent;
-            screenOp.innerHTML = x;
+            nmb1.innerHTML = x;
         }
     });
 
     $(".msc-btn").click(function(){
         if(this.innerHTML === "CLEAR"){
             x = "";
-            screenOp.innerHTML = x;
+            nmb1.innerHTML = x;
+            op.innerHTML = x;
+            nmb2.innerHTML = x;
+            res.innerHTML = "="
         }
         if(this.textContent === "ERASE"){
             x = x.toString();
             lastI = x.indexOf(" ");
             x = x.substring(0, x.length - 1);
-            screenOp.innerHTML = x;
+            x = parseInt(x);
+            nmb1.innerHTML = x;
         }
     });
 });
