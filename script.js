ba479d0de72fe90de4e2e581d13b712a999d0a5a
Lokowar <lokwoar1@gmail.com>
2022-07-07 16:04:57 -0300

    0.2 - Adicionei funcionalidade aos botões CLEAR e ERASE

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 $(document).ready(function() {
-    let x = ""
-    let screen = document.getElementById('screen');
+    let x = "";
+    let screenOp = document.getElementById('screen1');
+    let screenRes = document.getElementById('screen2');
+    let msc = document.getElementsByClassName("msc-btn");
 
     $(".btn").click(function(){
         if(typeof this.textContent === "string"){
             x += this.textContent;
-            screen.innerHTML = x;
+            x = parseInt(x);
+            screenOp.innerHTML = x;
+        }
+        if(this.textContent === "+"){
+            x += this.textContent;
+            screenOp.innerHTML = x;
+        }
+    });
 
-            console.log(x);
+    $(".msc-btn").click(function(){
+        if(this.innerHTML === "CLEAR"){
+            x = "";
+            screenOp.innerHTML = x;
+        }
+        if(this.textContent === "ERASE"){
+            x = x.toString();
+            lastI = x.indexOf(" ");
+            x = x.substring(0, x.length - 1);
+            screenOp.innerHTML = x;
         }
     });
 });
